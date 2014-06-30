7f7678d4ccf60e1b2403fe63ea52517433d3f30f
mz121star <mz121star@gmail.com>
2014-07-01 00:52:52 +0800

    ok

--- controller/login.php
+++ controller/login.php
@@ -1,9 +1,9 @@
 <?php
    error_reporting(E_ALL);
    ini_set('display_errors', '1');
-   echo 'a';
+   session_start();
 
-   include_once(../base/dbhelper.php);
+   include_once('../base/dbhelper.php');
    $wxid=$_POST["wxid"];
    $wxpwd=$_POST["wxpwd"];
    $dbhelper=new dbhelper();
