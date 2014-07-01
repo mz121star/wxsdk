3e8eb54557f410c7e5b196dd302acc7f6df3b5e7
mz121star <mz121star@gmail.com>
2014-07-01 10:13:49 +0800

    menu

--- controller/menu.php
+++ controller/menu.php
@@ -1,11 +1,13 @@
 <?php
-   //error_reporting(E_ALL);
-   //ini_set('display_errors', '1');
+   error_reporting(E_ALL);
+    ini_set('display_errors', '1');
 
-        session_start();
+    session_start();
    include_once('../base/dbhelper.php');
 
    $menustring=$_POST["menustring"];
+   echo    $menustring;
+   echo $_SESSION["uname"];
    $dbhelper=new dbhelper();
    $r=$dbhelper->updateMenu($_SESSION["uname"],urldecode($menustring));
 
